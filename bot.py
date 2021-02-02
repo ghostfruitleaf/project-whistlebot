51e8e4afdc4ed18680678ef360f63180198e99e2
PaulineChane <pauline.chane@gmail.com>
2021-02-01 20:09:59 -0800

    oauth issues when adding from other accounts

--- bot.py
+++ bot.py
@@ -1,30 +1,36 @@
 import settings
-import discord
+import hikari
+import lightbulb
 from pymongo import MongoClient
-from discord.ext import commands  # for another class object
+
 
 # setup database
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient(settings.CONNECTION_URL)
 db = client['whistlebot-db']
 
-# declare intents
-intents = discord.Intents.default()
-intents.members = True
+# DATABASE EVENTS
 
+def add_server(guild):
+    print("hi")
 
-# cog for command version of discord bot
-class Ping(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+bot = lightbulb.Bot(token=settings.DISCORD_BOT_TOKEN,
+                    prefix='!',
+                    insensitive_commands=True)
 
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send("pong")
+# flags a message by replying
+@bot.command()
+async def flag(ctx, reason=""):
+    msg_type = str(ctx.message.type)
+    if msg_type == 'DEFAULT':
+        await ctx.respond('**error:** no message attached. please reply to the message you wish to flag!')
+    elif msg_type == 'REPLY':
+        msg_ref = ctx.message.referenced_message
+        print(msg_ref.content)
+        if msg_ref.author.id in [int(settings.DISCORD_CLIENT_ID)]:
+            await ctx.respond('please do not attempt to circumvent whistlebot.')
+        elif ctx.message.author.id == msg_ref.author.id:
+            await ctx.respond('please do not attempt to abuse whistlebot functionality.')
+        else:
+            await ctx.message.add_reaction("\N{WHITE HEAVY CHECK MARK}")
 
-
-# bot script
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-bot.add_cog(Ping(bot))
-
-bot.run(settings.DISCORD_BOT_TOKEN)
+bot.run()
