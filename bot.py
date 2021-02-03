d37bcfe90d4e6573f12afe871c365fe56c36c205
PaulineChane <pauline.chane@gmail.com>
2021-02-02 20:04:15 -0800

    server startup function added to check for server profile

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ command discord bot for project-whistlebot to create reports and track authorize
 with whistlebot on discord. primarily creates new entries for users, servers, and reports in database.
 """
 import settings
+import hikari
 import lightbulb
 from pymongo import MongoClient
 
@@ -18,17 +19,17 @@ DATABASE EVENT METHODS
 creates new objects in whistlebot database.
 """
 
-"""
-add_server(guild)
-
-- accepts a discord Guild object to create a new server profile document in the database. 
-- triggered through report from a server with no standing reports or through addition of 
-  server in user interface. 
-"""
-
 
 # NOTE: NEEDS TO BE SECURED SOMEHOW??? HTTPS CONNECTION MAYBE?
 def add_server(guild):
+    """
+    add_server(guild)
+
+    - accepts a discord Guild object to create a new server profile document in the database.
+    - triggered through report from a server with no standing reports or through addition of
+      server in user interface.
+    - returns True if server profile added to database; false otherwise
+    """
     server_doc = {'doc_type': 'server',  # server doc
                   'server_name': guild.name,  # name of server
                   'server_id': int(guild.id),  # server id
@@ -37,33 +38,32 @@ def add_server(guild):
                   'flagger_roles': [],  # roles authorized to flag messages
                   'flag_tags': [],  # tags to attach to reports
                   'member_count': guild.member_count,  # num of members for stats
-                  'settings': {'kick_autoban': 0,  # number of kicks that trigger an autoban
+                  'settings': {'kick_autoban': None,  # number of kicks that trigger an autoban
                                'autoban_flags': [],  # flags that trigger autobans - REQUIRES DOC/VALIDATION
                                'user_agrmt_req': False,  # initialize user agreement requirement
-                               'user_agrmt_channel_id': int(guild.system_channel_id),  # channel for user agreement
-                               'user_agrmt_message_id': 0,  # message to check for agreement
+                               'user_agrmt_channel_id': int(guild.system_channel_id) if not guild.system_channel_id is None else 0,  # channel for user agreement
+                               'user_agrmt_message_id': None,  # message to check for agreement
                                'settings_roles': [],  # roles that can change server settings
                                'settings_users': [],  # roles that can change server users
                                'alert_roles': [],  # roles to be alerted of report
                                'alert_users': [],  # users to be alerted of report
-                               'alert_channel_id': 0  # id of channel to receive reports, if specified
+                               'alert_channel_id': None  # id of channel to receive reports, if specified
                                }
                   }
-    print(server_doc)
+    return True
 
 
-"""
-save_reported_message(reported_message)
+def save_reported_message(reported_message, report_id, reporter_id):
+    """
+    save_reported_message(reported_message)
 
-- creates and saves document for reported message as helper method of create_report.
-- does not create new reported_message if already in db, but increments times reported. 
-- returns True if saved to/found in database, False otherwise
-- ^ this allows another way to increase the priority of reports
-"""
-
-
-def save_reported_message(reported_message, report_id):
+    - creates and saves document for reported message as helper method of create_report.
+    - does not create new reported_message if already in db, but increments times reported.
+    - returns True if saved to/found in database, False otherwise
+    - ^ this allows another way to increase the priority of reports
+    """
     # if reported message in database it is not yet, increment times_reported and add report id to reports[]
+    # add check if a message is already reported
     # else
     reported_message = {  # info of REPORTED MESSAGE (should this be separated into another doc?)
         'doc_type': 'reported_message',
@@ -71,10 +71,12 @@ def save_reported_message(reported_message, report_id):
         'reported_user_id': reported_message.author.id,  # id of reported user (access)
         'reported_message': reported_message.content,  # content of ORIGINAL reported message
         'reported_embed': [o.url for o in reported_message.embeds],  # any embeds in message (NEED ARRAY OF OBJECTS)
-        'reported_attachments': [o.url for o in reported_message.attachments],  # any attachments in message (NEED ARRAY OF OBJECTS)
+        'reported_attachments': [o.url for o in reported_message.attachments],
+        # any attachments in message (NEED ARRAY OF OBJECTS)
         'reported_timestamp': reported_message.timestamp,  # time message sent
         'reported_edited_timestamp': reported_message.edited_timestamp,  # check for edit attempts
-        'reported_edits': [],  # array of edited messages as detected by bot on checks
+        'reported_edits': [] if reported_message.edited_timestamp is None else [reported_message.content],
+        # array of edited messages as detected by bot on checks
         'deleted': False,  # confirms if message is deleted
         'times_reported': 1,
         'reports': [report_id]
@@ -84,18 +86,16 @@ def save_reported_message(reported_message, report_id):
     return True
 
 
-"""
-create_report(report,reported_message)
-
-- creates report document in database using a message starting with !flag command
-  and message that is replied to 
-- triggered through authorized !flag command call from a user in a server
-- triggers creation of server document in database if not already existing
-- triggers creation of users if not already in database AFTER creation of report. 
-"""
-
-
 def create_report(report, reported_message):
+    """
+    create_report(report,reported_message)
+
+    - creates report document in database using a message starting with !flag command
+      and message that is replied to
+    - triggered through authorized !flag command call from a user in a server
+    - triggers creation of server document in database if not already existing
+    - triggers creation of users if not already in database AFTER creation of report.
+    """
     # generate report
     report_content = report.content.replace('!flag ', '', 1)
     flags = []
@@ -105,9 +105,9 @@ def create_report(report, reported_message):
     #     # via substring
 
     # report layout
-    if save_reported_message(reported_message, report.id):
+    if save_reported_message(reported_message, report.id, report.author.id):
 
-    # ALWAYS generate report if saved
+        # ALWAYS generate report if saved
         report = {'doc_type': 'report',  # indicates REPORT
                   'reviewed': [False, 0],  # indicates if report has been reviewed and by who?
                   'action': {'auth_user_id': 0,
@@ -119,7 +119,7 @@ def create_report(report, reported_message):
                   'reporter_user_id': report.author.id,  # id of reporter (for fast access)
                   'report_reason': report_content,  # reason for reporting
                   'flags': flags,  # array of flags identified from reason text
-                  'reported_message': reported_message.id
+                  'reported_message_id': reported_message.id
                   }
         print(report)
         # check that server is in database
@@ -170,15 +170,41 @@ ensures all users, reports, servers documented in database.
 may need to optimize as an ensure function? 
 """
 
+"""
+DISCORD BOT/METHODS
+
+ensures all users, reports, servers documented in database. 
+may need to optimize as an ensure function? 
+"""
+
 # initialize bot
 bot = lightbulb.Bot(token=settings.DISCORD_BOT_TOKEN,
                     prefix='!',
                     insensitive_commands=True)
 
+"""
+EVENT LISTENERS
+"""
+
+
+@bot.listen(hikari.GuildAvailableEvent)
+async def check_server_profile(event):
+    print(add_server(event.guild))
+
+
+"""
+COMMANDS
+"""
+
 
 # flags a message by replying
+@lightbulb.guild_only()
 @bot.command()
 async def flag(ctx):
+    """
+    Reply to a server message with !flag in order to report it.
+    Please note server moderation may require extra steps before you can use this function.
+    """
     msg_type = str(ctx.message.type)  # ensure the message is a reply
 
     # DM error if no reply found
@@ -223,6 +249,9 @@ async def flag(ctx):
 # DM user test
 @bot.command()
 async def ping(ctx):
+    """
+    Test function.
+    """
     await ctx.message.author.send('pong')
 
 
